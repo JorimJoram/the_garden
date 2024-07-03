777564c8c632fc7655b5fbcae95c3492125957ac
JorimJoram <wkdgyfla97@gmail.com>
2024-07-04 08:56:02 +0900

    Initial commit

---
+++ src/main/kotlin/com/sesac/climb_mates/ClimbMatesApplication.kt
@@ -0,0 +1,11 @@
+package com.sesac.climb_mates
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClimbMatesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClimbMatesApplication>(*args)
+}
---
+++ src/test/kotlin/com/sesac/climb_mates/ClimbMatesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sesac.climb_mates
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClimbMatesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
