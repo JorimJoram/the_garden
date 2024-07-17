297d6bd1789761b244471075e2046f387efec2b6
JorimJoram <wkdgyfla97@gmail.com>
2024-07-17 19:19:55 +0900

    긴급!!!] 메뉴 확인해줘!

--- src/main/resources/static/script/navi.js
+++ src/main/resources/static/script/navi.js
@@ -1,4 +1,36 @@
+var index = 0;
+let showing = false;
+
 function toLogin(){
-    console.log('test')
     window.location.href = "/login";
+}
+
+function setMenuList(){
+    var items = document.querySelectorAll('#navi_set li');
+
+    function showNextItem(){
+        if(index < items.length){
+            items[index].classList.remove('hidden_list');
+            index++;
+            setTimeout(showNextItem, 100); //시간값 잘 건드리면 더 자연스럽게 만들어질 수도 있음
+        }
+        showing = true;
+    }
+
+    function hideNextItem(){
+        if(index >= 0){
+            items[index].classList.add('hidden_list');
+            index--;
+            setTimeout(hideNextItem, 100);
+        }
+        showing = false;
+    }
+
+    if(!showing){
+        index = 0;
+        showNextItem();
+    }else{
+        index = items.length-1;
+        hideNextItem();
+    }
 }
